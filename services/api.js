298f2bcdfe33db82c06ec03be6c2fb9c60ea9fad
TranVanTienDat <ckim32993@gmail.com>
2025-09-06 22:31:23 +0700

    Import signOut

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getSession } from "next-auth/react";
+import { getSession, signOut } from "next-auth/react";
 
 export const isClient = () => typeof window !== "undefined";
 
